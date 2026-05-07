e8ed15d7f4f91ddd506bb7cd2f7b1e90ec080aca
Nagesh <C040625@caremarkrx.net>
2026-05-07 18:40:57 +0530

    Reading the excel v1

---
+++ src/main/java/com/example/ApachePoiPoc/ApachePoiPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApachePoiPoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachePoiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachePoiPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApachePoiPoc/ApachePoiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApachePoiPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachePoiPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
